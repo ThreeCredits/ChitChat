1723c439c529ca996d26d502d7a8a54363cd59a1
Simone Odierna <simoxodi@gmail.com>
2022-05-25 15:52:00 +0200

    Changes on e2ee.py
    feat: RSA keys now are exported as files

--- e2ee.py
+++ e2ee.py
@@ -1,4 +1,6 @@
 from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+import os
 
 
 class E2EE:
@@ -7,7 +9,23 @@ class E2EE:
         Generate a public/private key pair using 2048 bits key length
         :returns: None
         '''
-        self.key = RSA.generate(2048)
+        self.key = RSA.generate(2048) # generate a key pair
+        self.dir = '.' + get_random_bytes(16).hex() # create a directory with a random name
+        os.makedirs(self.dir) # create a directory with the random name
+        self.export_keys('keys') # export the keys as files
+    
+
+    def export_keys(self, file_name: str) -> None:
+        '''
+        Exports the private and public keys to a file
+        :param file_name: The name of the file
+        :returns: None
+        '''
+        with open(f'{self.dir}/{file_name}', 'wb') as f:
+            f.write(self.key.exportKey('PEM'))
+
+        with open(f'{self.dir}/{file_name}.pub', 'wb') as f:
+            f.write(self.key.publickey().exportKey('PEM'))
 
 
     def get_private_key(self) -> RSA.RsaKey:
@@ -15,7 +33,8 @@ class E2EE:
         Returns the private key
         :returns: Rsakey object
         '''
-        key = RSA.import_key(self.key.exportKey('PEM'))
+        with open(f'{self.dir}/keys', 'rb') as f:
+            key = RSA.import_key(f.read())
         return key
 
 
@@ -24,7 +43,8 @@ class E2EE:
         Returns the public key
         :returns: Rsakey object
         '''
-        key = RSA.import_key(self.key.publickey().exportKey('PEM'))
+        with open(f'{self.dir}/keys.pub', 'rb') as f:
+            key = RSA.import_key(f.read())
         return key
 
 
