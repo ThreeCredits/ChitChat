cd1368efe439008323ecb349874201c277c25d2e
Simone Odierna <simoxodi@gmail.com>
2022-11-08 23:46:07 +0100

    feat: added methods to JSON serialize and deserialize data from packets

--- cipher.py
+++ cipher.py
@@ -5,19 +5,79 @@ from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 import base64
 import json
+from message import *
+from datetime import datetime
 
+
+def serialize_(obj: Any) -> Any:
+    obj = list(obj)
+    for i in range(len(obj)):
+        obj[i] = base64.b64encode(obj[i]).decode('ascii')
+    obj = tuple(obj)
+    return json.dumps(obj, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+def serialize(obj: Packet) -> str:
+    for i in range(obj.data.__len__()):
+        if isinstance(obj.data[i].data, datetime):
+            obj.data[i].data = 'datetime'+obj.data[i].data.isoformat()
+        elif isinstance(obj.data[i].data, list) or isinstance(obj.data[i].data, tuple):
+            for j in range(obj.data[i].data.__len__()):
+                if isinstance(obj.data[i].data[j], datetime):
+                    obj.data[i].data[j] = 'datetime'+obj.data[i].data[j].isoformat()
+        elif isinstance(obj.data[i].data, dict):
+            for key, value in obj.data[i].data.items():
+                if isinstance(value, datetime):
+                    obj.data[i].data[key] = 'datetime'+value.isoformat()
+
+    return json.dumps(obj, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+def deserialize_(obj: Any) -> Any:
+    obj = json.loads(obj)
+    obj = list(obj)
+    for i in range(len(obj)):
+        obj[i] = base64.b64decode(obj[i])
+    return tuple(obj)
+
+def deserialize(obj: str) -> Packet:
+    obj_ = json.loads(obj)
+    items = []
+    for i in range(obj_['data'].__len__()):
+        type_ = obj_['data'][i]['type']
+        data = obj_['data'][i]['data']
+        if isinstance(data, list) or isinstance(data, tuple):
+            for i in range(data.__len__()):
+                if isinstance(data[i], str):
+                    if data[i].startswith('datetime'):
+                        data[i] = datetime.fromisoformat(data[i][8:])
+        elif isinstance(data, dict):
+            for key, value in data.items():
+                if isinstance(value, str):
+                    if value.startswith('datetime'):
+                        data[key] = datetime.fromisoformat(value[8:])
+        else:
+            if isinstance(data, str):
+                if data.startswith('datetime'):
+                    data = datetime.fromisoformat(data[8:])
+
+        items.append(PacketItem(type_, data))
+
+    return Packet(items)
 
 def send_ciphered_message(message, client, identity):
-    message = json.dumps(message)
+    message = serialize(message)
+    message = message.encode()
     message = identity.encrypt(message)
-    message = json.dumps(message)
-    client.send(message)
+    message = serialize_(message)
+    #client.send(message)
+    return message
 
 def receive_ciphered_message(message, client, identity):
-    message = client.recv(32 * 1024)
-    message = json.loads(message)
+    #message = client.recv(32 * 1024)
+    message = deserialize_(message)
     message = identity.decrypt(*message)
-    message = json.loads(message)
+    message = message.decode()
+    message = deserialize(message)
 
     return message
 
