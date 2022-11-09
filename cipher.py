9be979482d72fd7edee9e08a18be059ac034f799
Giovanni Maria Manduca <425gioviok@gmail.com>
2022-11-09 15:38:06 +0100

    fix: long messages auto-fragmentation handling

--- cipher.py
+++ cipher.py
@@ -1,3 +1,4 @@
+import struct
 from typing import Tuple
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
@@ -11,10 +12,35 @@ def send_ciphered_message(message, client, identity):
     message = pickle.dumps(message, protocol=5)
     message = identity.encrypt(message)
     message = pickle.dumps(message, protocol=5)
+    # Prefix each message with a 4-byte length (network byte order)
+    message = struct.pack('>I', len(message)) + message
     client.send(message)
 
+def receive_all(client, length):
+    data = bytearray()
+    while len(data) < length:
+        packet = client.recv(length - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+def _receive_chipered_message(client):
+    # Read message length and unpack it into an integer
+    try:
+        raw_msglen = receive_all(client, 4)
+        if not raw_msglen:
+            raise Exception("Missing message length header")
+    except:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return receive_all(client, msglen)
+
+
 def receive_ciphered_message(client, identity):
-    message = client.recv(1024 * 1024)
+    # Receive a large message, which may have been split into multiple packets
+    message = _receive_chipered_message(client)
     message = pickle.loads(message)
     message = identity.decrypt(*message)
     message = pickle.loads(message)
