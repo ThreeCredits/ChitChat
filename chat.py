977727835a223d488421fdbdcf17082330e9f9db
Bruno Montalto <brunomontaltoglok@gmail.com>
2022-11-09 23:34:39 +0100

    feat: local chats save and server comunication
    feat: comunication with server for receiving missing chats and chats where the user is no more in.
    
    fix: fixed the non focusing entry widget bug
    
    fix: messages scrolling
    
    feat: removed the possibility to send blank messages

--- chat.py
+++ chat.py
@@ -16,7 +16,7 @@ class ChatMessage:
         
 
 class Chat:
-    def __init__(self, id: int, chat_name: str, description: str, users: List[Tuple[str, int, int]] = [])-> None:
+    def __init__(self, id: int, chat_name: str, description: str, creation_date: datetime.datetime, users: List[Tuple[str, int, int]] = [])-> None:
         self.id = id
         self.chat_name = chat_name
         self.description = description
@@ -24,6 +24,12 @@ class Chat:
         self.messages = []
 
     
+    def update(self, chat_name: str, description: str, users: List[Tuple[str, int, int]]):
+        self.chat_name = chat_name
+        self.description = description
+        self.users = users
+
+
     def _append_message(self, msg_number: int , l: int, r: int) -> int:
         if l == r:
             return l
@@ -34,7 +40,7 @@ class Chat:
             return self._append_message(msg_number, l, p)
 
 
-    def append_message(self, msg: ChatMessage) -> None:
+    def append_message(self, msg: ChatMessage) -> None: #TODO
         self.messages.append(msg)
         return
         self.messages.insert(self._append_message(msg.number, 0, len(self.messages - 1)), msg)
